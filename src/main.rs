aaa81f9c1c302deaca06637e27660acbd1a38a94
Andrew O'Neil <andy@andyofniall.net>
2022-06-08 09:55:29 +1000

    Setup device with vulkano

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,133 @@
+use std::sync::Arc;
+
+use vulkano::{
+    device::{
+        physical::{PhysicalDevice, PhysicalDeviceType, QueueFamily},
+        Device, DeviceCreateInfo, DeviceExtensions, Features, QueueCreateInfo,
+    },
+    image::ImageUsage,
+    instance::{Instance, InstanceCreateInfo},
+    swapchain::{Surface, Swapchain, SwapchainCreateInfo},
+};
+use vulkano_win::VkSurfaceBuild;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let instance = Instance::new(InstanceCreateInfo {
+        enabled_extensions: vulkano_win::required_extensions(),
+        ..Default::default()
+    })
+    .unwrap();
+
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new()
+        .build_vk_surface(&event_loop, instance.clone())
+        .unwrap();
+
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::none()
+    };
+    let (physical_device, queue_family) =
+        select_physical_device(&instance, surface.clone(), &device_extensions);
+
+    println!(
+        "Using device: {} (type: {:?})",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type,
+    );
+
+    let (device, mut queues) = Device::new(
+        // Which physical device to connect to.
+        physical_device,
+        DeviceCreateInfo {
+            enabled_extensions: physical_device
+                .required_extensions()
+                .union(&device_extensions),
+            enabled_features: Features {
+                dynamic_rendering: true,
+                ..Features::none()
+            },
+            queue_create_infos: vec![QueueCreateInfo::family(queue_family)],
+            ..Default::default()
+        },
+    )
+    .unwrap();
+    let queue = queues.next().unwrap();
+
+    let (mut swapchain, images) = {
+        let surface_capabilities = physical_device
+            .surface_capabilities(&surface, Default::default())
+            .unwrap();
+
+        let image_format = Some(
+            physical_device
+                .surface_formats(&surface, Default::default())
+                .unwrap()[0]
+                .0,
+        );
+
+        Swapchain::new(
+            device.clone(),
+            surface.clone(),
+            SwapchainCreateInfo {
+                min_image_count: surface_capabilities.min_image_count,
+                image_format,
+                image_extent: surface.window().inner_size().into(),
+                image_usage: ImageUsage::color_attachment(),
+                composite_alpha: surface_capabilities
+                    .supported_composite_alpha
+                    .iter()
+                    .next()
+                    .unwrap(),
+                ..Default::default()
+            },
+        )
+        .unwrap()
+    };
+
+    event_loop.run(|event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        _ => (),
+    });
+
+    // let physical = PhysicalDevice::enumerate(&instance).next().expect("no device available");
+}
+
+fn select_physical_device<'a>(
+    instance: &'a Arc<Instance>,
+    surface: Arc<Surface<Window>>,
+    device_extensions: &DeviceExtensions,
+) -> (PhysicalDevice<'a>, QueueFamily<'a>) {
+    PhysicalDevice::enumerate(&instance)
+        .filter(|&device| {
+            device
+                .supported_extensions()
+                .is_superset_of(&device_extensions)
+        })
+        .filter_map(|device| {
+            device
+                .queue_families()
+                .find(|&family| {
+                    family.supports_graphics() && family.supports_surface(&surface).unwrap_or(false)
+                })
+                .map(|family| (device, family))
+        })
+        .min_by_key(|(device, _)| match device.properties().device_type {
+            PhysicalDeviceType::DiscreteGpu => 0,
+            PhysicalDeviceType::IntegratedGpu => 1,
+            PhysicalDeviceType::VirtualGpu => 2,
+            PhysicalDeviceType::Cpu => 3,
+            PhysicalDeviceType::Other => 4,
+        })
+        .unwrap()
 }
