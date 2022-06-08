d6f2ad7675304a7741f02d24053b8e725875187a
Andrew O'Neil <andy@andyofniall.net>
2022-06-08 16:26:22 +1000

    Create swapchain

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{ffi::CStr, os::raw::c_char};
 
-use ash::{extensions::khr::Surface, vk, Device, Entry, Instance};
+use ash::{
+    extensions::khr::{Surface, Swapchain},
+    vk, Device, Entry, Instance,
+};
 use env_logger::Env;
 use log::info;
 use winit::{
@@ -17,7 +20,9 @@ struct KeaApp {
     surface: vk::SurfaceKHR,
     _physical_device: vk::PhysicalDevice,
     device: Device,
-    _gfx_queue: vk::Queue,
+    _present_queue: vk::Queue,
+    swapchain_loader: Swapchain,
+    swapchain: vk::SwapchainKHR,
 }
 
 impl KeaApp {
@@ -29,12 +34,14 @@ impl KeaApp {
         let surface =
             unsafe { ash_window::create_surface(&entry, &instance, window, None) }.unwrap();
 
-        let (physical_device, gfx_queue_family_idx) = Self::select_physical_device(&instance);
-        let (device, gfx_queue) = Self::create_logical_device_with_queue(
-            &instance,
-            physical_device,
-            gfx_queue_family_idx,
-        );
+        let (physical_device, queue_family_index) =
+            Self::select_physical_device(&instance, surface, &surface_loader);
+        let (device, present_queue) =
+            Self::create_logical_device_with_queue(&instance, physical_device, queue_family_index);
+
+        let swapchain_loader = Swapchain::new(&instance, &device);
+        let swapchain =
+            Self::create_swapchain(surface, physical_device, &swapchain_loader, &surface_loader);
 
         KeaApp {
             _entry: entry,
@@ -43,13 +50,15 @@ impl KeaApp {
             surface,
             _physical_device: physical_device,
             device,
-            _gfx_queue: gfx_queue,
+            _present_queue: present_queue,
+            swapchain_loader,
+            swapchain,
         }
     }
 
     fn create_instance(entry: &Entry, window: &Window) -> Instance {
         let app_info = vk::ApplicationInfo::builder().api_version(vk::API_VERSION_1_3);
-        let extension_names = Self::extension_names(window);
+        let extension_names = Self::instance_extension_names(window);
 
         let layer_names = unsafe {
             [CStr::from_bytes_with_nul_unchecked(
@@ -70,22 +79,30 @@ impl KeaApp {
         unsafe { entry.create_instance(&create_info, None).unwrap() }
     }
 
-    fn extension_names(window: &Window) -> Vec<*const i8> {
+    fn instance_extension_names(window: &Window) -> Vec<*const i8> {
         ash_window::enumerate_required_extensions(window)
             .unwrap()
             .to_vec()
     }
 
-    fn select_physical_device(instance: &Instance) -> (vk::PhysicalDevice, u32) {
+    fn device_extension_names() -> Vec<*const i8> {
+        vec![Swapchain::name().as_ptr()]
+    }
+
+    fn select_physical_device(
+        instance: &Instance,
+        surface: vk::SurfaceKHR,
+        surface_loader: &Surface,
+    ) -> (vk::PhysicalDevice, u32) {
         let devices = unsafe { instance.enumerate_physical_devices() }.unwrap();
-        let (device, gfx_queue_family_idx) = devices
+        let (device, queue_family_index) = devices
             .into_iter()
-            .find_map(
-                |device| match Self::find_gfx_queue_family_idx(instance, device) {
+            .find_map(|device| {
+                match Self::find_queue_family_idx(instance, device, surface, surface_loader) {
                     Some(idx) => Some((device, idx)),
                     None => None,
-                },
-            )
+                }
+            })
             .unwrap();
 
         let props = unsafe { instance.get_physical_device_properties(device) };
@@ -93,20 +110,31 @@ impl KeaApp {
             CStr::from_ptr(props.device_name.as_ptr())
         });
 
-        (device, gfx_queue_family_idx)
+        (device, queue_family_index)
     }
 
-    fn find_gfx_queue_family_idx(
+    fn find_queue_family_idx(
         instance: &Instance,
         physical_device: vk::PhysicalDevice,
+        surface: vk::SurfaceKHR,
+        surface_loader: &Surface,
     ) -> Option<u32> {
         let props =
             unsafe { instance.get_physical_device_queue_family_properties(physical_device) };
         props
             .iter()
             .enumerate()
-            .find(|(_, family)| {
-                family.queue_count > 0 && family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+            .find(|(index, family)| {
+                family.queue_count > 0
+                    && family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+                    && unsafe {
+                        surface_loader.get_physical_device_surface_support(
+                            physical_device,
+                            *index as u32,
+                            surface,
+                        )
+                    }
+                    .unwrap()
             })
             .map(|(index, _)| index as _)
     }
@@ -114,19 +142,62 @@ impl KeaApp {
     fn create_logical_device_with_queue(
         instance: &Instance,
         physical_device: vk::PhysicalDevice,
-        gfx_queue_family_idx: u32,
+        queue_family_index: u32,
     ) -> (Device, vk::Queue) {
         let queue_create_infos = [vk::DeviceQueueCreateInfo::builder()
-            .queue_family_index(gfx_queue_family_idx)
+            .queue_family_index(queue_family_index)
             .queue_priorities(&[1.0])
             .build()];
+        let extension_names = Self::device_extension_names();
+        let create_info = vk::DeviceCreateInfo::builder()
+            .queue_create_infos(&queue_create_infos)
+            .enabled_extension_names(&extension_names);
 
-        let create_info = vk::DeviceCreateInfo::builder().queue_create_infos(&queue_create_infos);
         let device =
             unsafe { instance.create_device(physical_device, &create_info, None) }.unwrap();
-        let graphics_queue = unsafe { device.get_device_queue(gfx_queue_family_idx, 0) };
+        let present_queue = unsafe { device.get_device_queue(queue_family_index, 0) };
 
-        (device, graphics_queue)
+        (device, present_queue)
+    }
+
+    fn create_swapchain(
+        surface: vk::SurfaceKHR,
+        physical_device: vk::PhysicalDevice,
+        swapchain_loader: &Swapchain,
+        surface_loader: &Surface,
+    ) -> vk::SwapchainKHR {
+        let surface_capabilities = unsafe {
+            surface_loader.get_physical_device_surface_capabilities(physical_device, surface)
+        }
+        .unwrap();
+
+        let image_count = surface_capabilities.min_image_count + 1;
+        let image_count = if surface_capabilities.max_image_count > 0 {
+            image_count.min(surface_capabilities.max_image_count)
+        } else {
+            image_count
+        };
+
+        let surface_format =
+            unsafe { surface_loader.get_physical_device_surface_formats(physical_device, surface) }
+                .unwrap()[0];
+
+        let swapchain_create_info = vk::SwapchainCreateInfoKHR::builder()
+            .surface(surface)
+            .min_image_count(image_count)
+            .image_color_space(surface_format.color_space)
+            .image_format(surface_format.format)
+            .image_extent(vk::Extent2D {
+                width: 1920,
+                height: 1080,
+            })
+            .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+            .pre_transform(surface_capabilities.current_transform)
+            .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+            .image_array_layers(1)
+            .present_mode(vk::PresentModeKHR::MAILBOX);
+
+        unsafe { swapchain_loader.create_swapchain(&swapchain_create_info, None) }.unwrap()
     }
 
     pub fn run(self, event_loop: EventLoop<()>, _window: Window) {
@@ -145,6 +216,8 @@ impl KeaApp {
 impl Drop for KeaApp {
     fn drop(&mut self) {
         unsafe {
+            self.swapchain_loader
+                .destroy_swapchain(self.swapchain, None);
             self.surface_loader.destroy_surface(self.surface, None);
             self.device.destroy_device(None);
             self.instance.destroy_instance(None);
