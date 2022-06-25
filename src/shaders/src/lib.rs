9bce02cdee67f37293be866bb967705d8558f64d
Andrew O'Neil <andy@andyofniall.net>
2022-06-25 13:11:39 +1000

    WIP

--- src/shaders/src/lib.rs
+++ src/shaders/src/lib.rs
@@ -4,14 +4,14 @@
     feature(register_attr),
     register_attr(spirv)
 )]
-#![deny(warnings)]
+// #![deny(warnings)]
 
 #[cfg(not(target_arch = "spirv"))]
 use spirv_std::macros::spirv;
 
 use spirv_std::{
-    glam::{vec4, UVec2, UVec3, Vec2, Vec3, Vec4},
-    ray_tracing::AccelerationStructure,
+    glam::{vec3, vec4, UVec2, UVec3, Vec2, Vec3, Vec4},
+    ray_tracing::{AccelerationStructure, RayFlags},
     Image,
 };
 
@@ -19,6 +19,11 @@ use spirv_std::{
 pub struct Vertex {
     pub position: Vec2,
     pub color: Vec3,
+}
+
+#[repr(C)]
+pub struct RayPayload {
+    color: Vec3,
 }
 
 #[spirv(vertex)]
@@ -40,22 +45,58 @@ pub fn main_fragment(fragment_color: Vec3, output: &mut Vec4) {
 #[spirv(ray_generation)]
 pub fn generate_rays(
     #[spirv(launch_id)] launch_id: UVec3,
-    #[spirv(descriptor_set = 0, binding = 0)] _accel_structure: &AccelerationStructure,
+    #[spirv(descriptor_set = 0, binding = 0)] accel_structure: &AccelerationStructure,
     #[spirv(descriptor_set = 0, binding = 1)] image: &mut Image!(2D, format=rgba32f, sampled=false),
 ) {
+    let ray_direction = ray_for_pixel(launch_id.x, launch_id.y);
+    let mut payload = RayPayload {
+        color: vec3(0.0, 0.0, 0.0),
+    };
+
     unsafe {
+        // accel_structure.trace_ray(
+        //     RayFlags::OPAQUE,
+        //     0xff,
+        //     0,
+        //     0,
+        //     0,
+        //     vec3(0.0, 0.0, 0.0),
+        //     0.001,
+        //     ray_direction,
+        //     10000.0,
+        //     &mut payload,
+        // );
+
         image.write(
             UVec2::new(launch_id.x, launch_id.y),
-            vec4(0.5, 0.5, 0.5, 1.0),
+            vec4(payload.color.x, payload.color.y, payload.color.z, 1.0),
         );
     }
 }
 
+pub fn ray_for_pixel(x: u32, y: u32) -> Vec3 {
+    let width = 1920;
+    let height = 1080;
+    let aspect_ratio = width as f32 / height as f32;
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let u = x as f32 / width as f32;
+    let v = y as f32 / height as f32;
+    vec3(u * viewport_width, v * viewport_height, focal_length)
+}
+
 #[spirv(miss)]
-pub fn ray_miss() {}
+pub fn ray_miss(#[spirv(incoming_ray_payload)] ray_payload: &mut RayPayload) {
+    ray_payload.color = vec3(0.0, 0.0, 1.0);
+}
 
 #[spirv(closest_hit)]
-pub fn ray_hit() {}
+pub fn ray_hit(#[spirv(incoming_ray_payload)] ray_payload: &mut RayPayload) {
+    ray_payload.color = vec3(1.0, 0.0, 0.0);
+}
 
 #[spirv(intersection)]
 pub fn intersect_sphere() {}
