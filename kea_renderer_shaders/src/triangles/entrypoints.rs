0cdb6d7215a94fcb4f1b8cc50cab03dd168f1899
Andrew O'Neil <andy@andyofniall.net>
2022-07-21 08:21:46 +1000

    Ensure triangle normals are in the ray direction

--- kea_renderer_shaders/src/triangles/entrypoints.rs
+++ kea_renderer_shaders/src/triangles/entrypoints.rs
@@ -3,11 +3,12 @@ use spirv_std::macros::spirv;
 
 use super::Mesh;
 use crate::payload::RayPayload;
-use spirv_std::glam::{vec3, Vec3A};
+use spirv_std::glam::{vec3, Vec3, Vec3A};
 
 #[spirv(closest_hit)]
 pub fn triangle_hit(
     #[spirv(ray_tmax)] hit_max: f32,
+    #[spirv(world_ray_direction)] ray_direction: Vec3,
     #[spirv(incoming_ray_payload)] ray_payload: &mut RayPayload,
     #[spirv(storage_buffer, descriptor_set = 0, binding = 3)] meshes: &[Mesh],
     #[spirv(storage_buffer, descriptor_set = 0, binding = 4)] vertices: &[Vec3A],
@@ -25,11 +26,15 @@ pub fn triangle_hit(
 
     let u = points[1] - points[0];
     let v = points[2] - points[0];
-    let normal = vec3(
+    let mut normal = vec3(
         u.y * v.z - u.z * v.y,
         u.z * v.x - u.x * v.z,
         u.x * v.y - u.y * v.x,
     );
+
+    if ray_direction.dot(normal) > 0.0 {
+        normal = normal * -1.0;
+    }
 
     *ray_payload = RayPayload {
         hit: Some(hit_max),
